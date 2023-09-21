84f8dc2b7732e0b2e81dc910d207334e14a2b6fc
mxrgxr <mgxrcias@gmail.com>
2023-09-20 19:03:22 -0700

    write task methods

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,10 +1,28 @@
 class TaskController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
   end
 end
