bf3e9314d262f69e2f76b61f22e41ae69f84c94f
mxrgxr <mgxrcias@gmail.com>
2023-09-20 18:58:46 -0700

    add task routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'task/index'
-  get 'task/create'
-  get 'task/update'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks, only: [:index, :create, :update]
 end
