cb95065772a932d81c18bb3b510d3f646c8d0605
mxrgxr <mgxrcias@gmail.com>
2023-09-21 11:16:56 -0700

    rspec request and model validation tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [:index, :new, :create, :edit, :update]
+  resources :tasks
   root to: "tasks#index"
 end
--- spec/requests/task_spec.rb
+++ spec/requests/task_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/task/index"
+      get tasks_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
+  describe "GET /new" do
     it "returns http success" do
-      get "/task/create"
+      get new_task_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
+  describe "GET /edit" do
     it "returns http success" do
-      get "/task/update"
+      task = FactoryBot.create(:task)
+      get edit_task_path(task)
       expect(response).to have_http_status(:success)
     end
   end
-
 end
